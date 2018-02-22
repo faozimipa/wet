b5da3e2b37f58a171630a93dbbb6d0c8dc1a0ff6
FAOZI <faozimipa@gmail.com>
2018-02-22 16:26:16 +0700

    first commit

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+'use strict';
+
+importScripts('./build/sw-toolbox.js');
+
+self.toolbox.options.cache = {
+  name: 'ionic-cache'
+};
+
+// pre-cache our key assets
+self.toolbox.precache(
+  [
+    './build/main.js',
+    './build/vendor.js',
+    './build/main.css',
+    './build/polyfills.js',
+    'index.html',
+    'manifest.json'
+  ]
+);
+
+self.toolbox.router.any('/*', self.toolbox.cacheFirst);
+
+self.toolbox.router.default = self.toolbox.networkFirst;
